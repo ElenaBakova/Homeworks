f28116b93b159549d47b34766270084b547a48f4
ElenaBakova <lenabakova72@gmail.com>
2020-09-24 20:24:54 +0300

    k

---
+++ Third homework/Sorts/ProjectSorts/Source.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int* block; 
+    int n = 0;
+    int m = 0;
+    scanf("%d %d", &n, &m);
+    n -= m;
+    block = malloc((n) * sizeof(int));
+
+    for (int i = 0;i < n; i++)
+    {
+        printf("block[%d]=", i);
+        scanf("%d", &block[i]);
+    }
+    printf("\n");
+
+    for (int i = 0;i < n; i++)
+    {
+        printf("%d \t", block[i]);
+    }
+
+    free(block);
+
+    return 0;
+}
