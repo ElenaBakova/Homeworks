c454f8cbf26bfc36ec439426426495b2f0435963
ElenaBakova <lenabakova72@gmail.com>
2020-10-06 12:26:15 +0300

    Added problem2 test1

---
+++ Test 1/Project2/Source.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+	FILE* input = fopen("input.txt", "r");
+	if (input == NULL)
+	{
+		printf("File not found");
+		return 0;
+	}
+	FILE* output = fopen("output.txt", "w");
+	if (output == NULL)
+	{
+		return 0;
+	}
+	
+	int countSymbol[128] = { 0 };
+	while (!feof(input))
+	{
+		int symbolCode = getc(input);
+		if (symbolCode >= 0)
+		{
+			countSymbol[symbolCode]++;
+		}
+	}
+	int count = 0;
+	for (int i = 0; i < 128; i++)
+	{
+		if (countSymbol[i] > 1)
+		{
+			count++;
+			fprintf(output, "'%c' meets %i times\n", i, countSymbol[i]);
+		}
+		else if (countSymbol[i] == 1)
+		{
+			count++;
+			fprintf(output, "'%c' meets 1 time\n", i);
+		}
+	}
+	if (count == 0)
+	{
+		fprintf(output, "File is empty");
+	}
+	
+	fclose(input);
+	fclose(output);
+	return 0;
+}
