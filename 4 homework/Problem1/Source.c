85bcf9b9b271ac4985f111442f8d9eae6c9b56a8
ElenaBakova <lenabakova72@gmail.com>
2020-10-14 00:58:34 +0300

    Added solution to hw 4 problem 1

---
+++ 4 homework/Problem1/Source.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+#include <stdbool.h>
+
+int decToBin(int array[], int number)
+{
+	int i = 0;
+	while (number > 1)
+	{
+		array[i] = number & 1 ? 1 : 0;
+		number >>= 1;
+		i++;
+	}
+	array[i] = number;
+	return ++i;
+}
+
+int binToDec(const int array[], const int size)
+{
+	int decimal = 0;
+	int power = 1;
+	for (int i = 0; i < size; i++)
+	{
+		decimal += array[i] * power;
+		power <<= 1;
+	}
+	return decimal;
+}
+
+
+void twosComplement(int array[], int *size)
+{
+	for (int i = 0; i < *size; i++)
+	{
+		array[i] ^= 1;
+	}
+	int j = 0;
+	array[0] += 1;
+	while(j < *size)
+	{
+		int temp = array[j];
+		array[j] %= 2;
+		array[j + 1] += temp / 2;
+		j++;
+	}
+	if (array[j] == 1)
+	{
+		(*size)++;
+	}
+}
+
+void addition(int first[], int second[], int result[], const int size)
+{
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n = 0;
+	int k = 0;
+	printf("¬ведите два числа: ");
+	scanf("%i %i", &n, &k);
+	int binaryK[20] = { 0 };
+	int binaryN[20] = { 0 };
+	int nSize = decToBin(binaryN, abs(n));
+	int kSize = decToBin(binaryK, abs(k));
+	if (n < 0)
+	{
+		twosComplement(binaryN, &nSize);
+	}
+	if (k < 0)
+	{
+		twosComplement(binaryK, &kSize);
+	}
+	int resultArray[20] = { 0 };
+	/* for (int i = 0; i < nSize; i++)
+		printf("%i", binaryN[i]);
+	printf("%i", binToDec(binaryN, nSize));*/
+
+	return 0;
+}
