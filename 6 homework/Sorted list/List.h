ea9798a01122ee22d7d2d9cb9a6cf44b878b2de1
ElenaBakova <lenabakova72@gmail.com>
2020-11-04 15:20:58 +0300

    Amended file

--- 6 homework/Sorted list/List.h
+++ 6 homework/Sorted list/List.h
@@ -6,7 +6,7 @@ typedef struct List List;
 // Returns value of the item
 int getTheValue(List *list);
 
-// changes pointer to the next item
+// Changes pointer to the next item
 void nextItem(List* list);
 
 // Makes a new list
@@ -18,10 +18,10 @@ void addItem(List* list, const int value);
 // Removes elment from the list. Recieves value of the element
 bool removeValue(List* list, const int value);
 
-// Returns 'true' if stack is emty
+// Returns 'true' if list is emty
 bool isEmpty(List* list);
 
-// Removes all stack
+// Removes all list
 void freeList(List** list);
 
 // Prints all list
