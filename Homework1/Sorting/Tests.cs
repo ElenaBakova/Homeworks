2f6d7d67d5a45647dd4a69b5b0f44f2d74736b27
ElenaBakova <lenabakova72@gmail.com>
2021-02-13 23:01:04 +0300

    Added 'Tests.cs'

---
+++ Homework1/Sorting/Tests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Sorting
+{
+    class Tests
+    {
+        public static bool Test()
+        {
+            const int size = 10;
+            int[] array = MakeArray(size);
+            Program.Sort(array);
+            bool result = true;
+            for (int i = 1; i < size; i++)
+            {
+                result &= (array[i - 1] < array[i]);
+            }
+            return result;
+        }
+
+        private static int[] MakeArray(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Random random = new Random();
+                array[i] = random.Next();
+            }
+            return array;
+        }
+    }
+}
