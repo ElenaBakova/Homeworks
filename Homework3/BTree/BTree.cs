bb13688efd20f7f59ac44a27bd5aafff5273ad4a
ElenaBakova <lenabakova72@gmail.com>
2021-03-07 17:36:23 +0300

    Added files

---
+++ Homework3/BTree/BTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTree
+{
+    /// <summary>
+    /// Tree wich contains key(string) and value(string)
+    /// </summary>
+    class BTree
+    {
+        int treeOrder;
+        Node root;
+
+        class Node
+        {
+            public int countNodes;
+            public string[] keys;
+            public Node[] child;
+            public bool isLeaf;
+
+            public Node(int order)
+            {
+                keys = new string[2 * order - 1];
+                child = new Node[2 * order];
+                isLeaf = true;
+            }
+        }
+
+        public BTree(int order)
+        {
+            treeOrder = order;
+            root = new Node(order);
+        }
+    }
+}
---
+++ Homework3/BTree/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please define order of the tree");
+            string read = Console.ReadLine();
+            string[] numbers = read.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var treeOrder = int.Parse(numbers[0]);
+        }
+    }
+}
