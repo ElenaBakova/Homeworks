65fc9e28b62252c0502d550fea8b20858fc052a8
ElenaBakova <lenabakova72@gmail.com>
2021-03-19 22:10:42 +0300

    Added files

---
+++ Semester2/Homework4/UniqueList/List.cs
@@ -0,0 +1,37 @@
+﻿namespace UniqueList
+{
+    /// <summary>
+    /// Contains integer values
+    /// </summary>
+    class List
+    {
+        private class ListElement
+        {
+            public ListElement(int value, ListElement next)
+            {
+                Value = value;
+                Next = next;
+            }
+
+            public int Value { get; set; }
+
+            public ListElement Next { get; set; }
+        }
+
+        private ListElement head;
+
+        /// <summary>
+        /// Returns true if list is empty.
+        /// </summary>
+        public bool Empty => head == null;
+
+        /// <summary>
+        /// Adds value into list
+        /// </summary>
+        /// <param name="value">New value</param>
+        public void Add(int value, int position)
+        {
+            
+        }
+    }
+}
