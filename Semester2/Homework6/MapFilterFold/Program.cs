f58b31545efe8b25073eb3658d7bba90cfd74f59
ElenaBakova <lenabakova72@gmail.com>
2021-03-25 23:15:31 +0300

    Added files

---
+++ Semester2/Homework6/MapFilterFold/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace MapFilterFold
+{
+    class Program
+    {
+        public List<int> MapFilterFold(List<int> list, [Optional] int value, Func<int, T> function)
+        {
+            var answerList = new List<int>();
+            foreach (var element in list)
+            {
+                var functionResult = function(element);
+                if (functionResult is int)
+                {
+                    answerList.Add(functionResult);
+                }
+                if (functionResult is bool && functionResult)
+                {
+                    answerList.Add(element);
+                }
+            }
+            return answerList;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
