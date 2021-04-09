bea3f1b3d8d4d8743f1f8a742de265c2b90703ca
ElenaBakova <lenabakova72@gmail.com>
2021-04-09 22:26:08 +0300

    Init

---
+++ Semester2/Homework5/Routers/DSU.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace Routers
+{
+    class DSU
+    {
+        private Dictionary<int, int> parent;
+        private Dictionary<int, int> rank;
+
+        public void Dsu()
+        {
+            parent = new();
+            rank = new();
+        }
+
+        public void MakeSet(int vertex)
+        {
+            parent.Add(vertex, vertex);
+            rank.Add(vertex, 0);
+        }
+
+        public int FindSet(int vertex)
+        {
+            if (vertex == parent[vertex])
+            {
+                return vertex;
+            }
+            return parent[vertex] = FindSet(parent[vertex]);
+        }
+
+        private void Swap(ref int first, ref int second)
+        {
+            int temp = first;
+            first = second;
+            second = temp;
+        }
+
+        public void UnionSet(int first, int second)
+        {
+            first = FindSet(first);
+            second = FindSet(second);
+            if (first == second)
+            {
+                return;
+            }
+            if (rank[first] < rank[second])
+            {
+                Swap(ref first, ref second);
+            }
+            parent[second] = first;
+            if (rank[first] == rank[second])
+            {
+                rank[first]++;
+            }
+        }
+    }
+}
---
+++ Semester2/Homework5/Routers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Routers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
