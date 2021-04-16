92a27ea31b8415bd21d50df73fdfae0994f84356
ElenaBakova <lenabakova72@gmail.com>
2021-04-16 20:48:52 +0300

    Deleted Main'

--- Semester2/Homework6/MapFilterFold/Program.cs
+++ Semester2/Homework6/MapFilterFold/Program.cs
@@ -6,8 +6,7 @@ namespace MapFilterFold
     {
         static void Main(string[] args)
         {
-            //var list = MapFilterFold.Map(new List<int>() { 1, 2, 3 }, x => x * 2);
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
