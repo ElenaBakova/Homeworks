9ea4d47fa9417f69aa0afbe1bf5ef208892797e1
ElenaBakova <lenabakova72@gmail.com>
2021-04-16 11:32:55 +0300

    Added files

---
+++ Semester2/Test/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    /// <summary>
+    /// Priority queue class
+    /// </summary>
+    /// <typeparam name="TValue">Type of the value</typeparam>
+    class PriorityQueue<TValue>
+    {
+        /// <summary>
+        /// Priority queue element class 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class QueueElement<T>
+        {
+            public QueueElement(T value, int priority, QueueElement<T> next)
+            {
+                Value = value;
+                Priority = priority;
+                Next = next;
+            }
+
+            public T Value { get; set; }
+            public int Priority { get; set; }
+            public QueueElement<T> Next { get; set; }
+        }
+
+    }
+}
