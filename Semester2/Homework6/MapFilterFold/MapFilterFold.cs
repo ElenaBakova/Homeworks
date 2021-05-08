75783d352dbcf0aec27c0a71c057cf865305f112
ElenaBakova <lenabakova72@gmail.com>
2021-05-08 18:27:22 +0300

    Renamed params

--- Semester2/Homework6/MapFilterFold/MapFilterFold.cs
+++ Semester2/Homework6/MapFilterFold/MapFilterFold.cs
@@ -8,12 +8,12 @@ namespace MapFilterFold
         /// <summary>
         /// Changes each element from list using function
         /// </summary>
-        /// <typeparam name="T">type of the elements</typeparam>
+        /// <typeparam name="TInput">type of the elements</typeparam>
         /// <param name="function">function that changes element in list</param>
         /// <returns>New list</returns>
-        public static List<U> Map<T, U>(List<T> list, Func<T, U> function)
+        public static List<TOutput> Map<TInput, TOutput>(List<TInput> list, Func<TInput, TOutput> function)
         {
-            var answerList = new List<U>();
+            var answerList = new List<TOutput>();
             foreach (var element in list)
             {
                 answerList.Add(function(element));
@@ -26,7 +26,7 @@ namespace MapFilterFold
         /// </summary>
         /// <param name="function">Recieves element, returns true or false</param>
         /// <returns>New list</returns>
-        public static List<T> Filter<T>(List<T> list, Func<T, bool> function)
+        public static List<TInput> Filter<TInput>(List<TInput> list, Func<TInput, bool> function)
             => list.FindAll(i => function(i));
         
         /// <summary>
@@ -34,7 +34,7 @@ namespace MapFilterFold
         /// </summary>
         /// <param name="value">starting value</param>
         /// <returns>Accumulated result</returns>
-        public static U Fold<T, U>(List<T> list, U value, Func<U, T, U> function)
+        public static TOutput Fold<TInput, TOutput>(List<TInput> list, TOutput value, Func<TOutput, TInput, TOutput> function)
         {
             foreach (var element in list)
             {
