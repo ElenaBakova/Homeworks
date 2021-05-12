62432f8584da821dcbfea22eeb94b32cfe01978d
ElenaBakova <lenabakova72@gmail.com>
2021-05-12 16:34:14 +0300

    First commit

---
+++ Semester2/Homework7/Clock/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Clock
+{
+    partial class ClockForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClockBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ClockBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ClockBox
+            // 
+            this.ClockBox.BackColor = System.Drawing.SystemColors.Window;
+            this.ClockBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ClockBox.Location = new System.Drawing.Point(0, 0);
+            this.ClockBox.Name = "ClockBox";
+            this.ClockBox.Size = new System.Drawing.Size(482, 453);
+            this.ClockBox.TabIndex = 0;
+            this.ClockBox.TabStop = false;
+            // 
+            // ClockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.ClockBox);
+            this.Name = "ClockForm";
+            this.Text = "ClockForm";
+            this.Load += new System.EventHandler(this.ClockForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ClockBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ClockBox;
+    }
+}
+
---
+++ Semester2/Homework7/Clock/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class ClockForm : Form
+    {
+        public ClockForm()
+        {
+            InitializeComponent();
+        }
+
+        private int clockWidth = 500;
+        private int clockHeight = 500;
+        //private PictureBox ClockBox = new PictureBox();
+        private Font font = new Font("Arial", 14);
+
+        private void ClockForm_Load(object sender, EventArgs e)
+        {
+            ClockBox.Paint += new PaintEventHandler(ClockPaint);
+
+            //Controls.Add(ClockBox);
+        }
+
+        private void ClockPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            g.DrawEllipse(new Pen(Brushes.Black, 10), 0, 0, clockWidth, clockHeight);
+        }
+    }
+}
