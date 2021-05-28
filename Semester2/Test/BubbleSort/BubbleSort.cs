62b292b8dcee935a412156d909084b6a3b616e14
ElenaBakova <lenabakova72@gmail.com>
2021-05-28 11:40:35 +0300

    Added files

---
+++ Semester2/Test/BubbleSort/BubbleSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BubbleSort
+{
+	/// <summary>
+	/// Class for generic bubble sort
+	/// </summary>
+	class BubbleSort
+    {
+		/// <summary>
+		/// Sorting method
+		/// </summary>
+		/// <typeparam name="TValue">The type of elements in the list</typeparam>
+		/// <param name="list">List that should be sorted</param>
+		/// <param name="comparator">Function for comparing two elements</param>
+        public void Sorting<TValue>(List<TValue> list, Func<TValue, TValue, int> comparator)
+        {
+			int size = list.Count;
+			for (int i = 0; i < size - 1; ++i)
+			{
+				for (int j = 0; j < size - i - 1; ++j)
+				{
+					if (comparator(list[j], list[j + 1]) > 0)
+					{
+						var temp = list[j];
+						list[j] = list[j + 1];
+						list[j + 1] = temp;
+					}
+				}
+			}
+		}
+    }
+}
