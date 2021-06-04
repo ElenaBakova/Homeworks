596eb62b100c16ea0124665c42d5430d8d13906a
ElenaBakova <lenabakova72@gmail.com>
2021-06-04 13:10:38 +0300

    Changed

--- Semester2/Test/RLEAlgorithm.Tests/RLETest.cs
+++ Semester2/Test/RLEAlgorithm.Tests/RLETest.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-using System.Collections.Generic;
+using System.Text;
 using static RLEAlgorithm.RLE;
 
 namespace RLEAlgorithm.Tests
@@ -11,7 +11,7 @@ namespace RLEAlgorithm.Tests
     {
         [TestCase("WWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWBWWWWWWWWWWWWWW", ExpectedResult = "9W3B24W1B14W")]
         [TestCase("ABCABCABCDDDFFFFFF", ExpectedResult = "1A1B1C1A1B1C1A1B1C3D6F")]
-        public string CompressionTest(byte[] input)
-            => ConvertToString(Compress(input));
+        public string CompressionTest(string input)
+            => ConvertToString(Compress(Encoding.ASCII.GetBytes(input)));
     }
 }
