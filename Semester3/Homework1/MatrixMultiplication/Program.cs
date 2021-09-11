9d6085bea3d8ef787e9fb4f5e5fad702ef1ee21f
ElenaBakova <lenabakova72@gmail.com>
2021-09-11 15:05:22 +0300

    Added files

---
+++ Semester3/Homework1/MatrixMultiplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace MatrixMultiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstMatrixPath;
+            string secoindMatrixPath;
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Couldn't find input files path\nPlease enter Matrix A path");
+                firstMatrixPath = Console.ReadLine();
+                Console.WriteLine("Please enter Matrix B path");
+                secoindMatrixPath = Console.ReadLine();
+            }
+            else
+            {
+                firstMatrixPath = args[0];
+                secoindMatrixPath = args[1];
+            }
+
+            using (StreamReader stream = File.OpenText(firstMatrixPath))
+            {
+
+            }
+        }
+    }
+}
