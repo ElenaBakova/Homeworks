94116557555906c1b22c392a5e0ac6c1fd1efc31
ElenaBakova <lenabakova72@gmail.com>
2021-09-26 00:19:50 +0300

    Added tests

--- Semester3/Homework2/Lazy.Tests/LazyTests.cs
+++ Semester3/Homework2/Lazy.Tests/LazyTests.cs
@@ -1,5 +1,7 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lazy.Tests
 {
@@ -10,6 +12,28 @@ namespace Lazy.Tests
         {
             Assert.Throws<ArgumentNullException>(() => LazyFactory.CreateSingleThreadedLazy<int>(null));
             Assert.Throws<ArgumentNullException>(() => LazyFactory.CreateMultiThreadedLazy<int>(null));
+        }
+
+        private static object[] testData = new[]
+        {
+            2,
+            10,
+            "aabacaba",
+            "bbaa" as object
+        };
+
+        private static IEnumerable<TestCaseData> Lazies
+            => testData.SelectMany(data => new TestCaseData[]
+               {
+                    new TestCaseData(LazyFactory.CreateSingleThreadedLazy(() => data)),
+                   new TestCaseData(LazyFactory.CreateMultiThreadedLazy(() => data))
+               });
+
+        [TestCaseSource(nameof(Lazies))]
+        public void GetShouldNotChangeValue<T>(ILazy<T> lazy)
+        {
+            var value = lazy.Get();
+            Assert.Equals(value, lazy.Get());
         }
     }
 }
