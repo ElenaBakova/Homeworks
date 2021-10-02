bc1945ed24c8aa5eb1bf0a1cd2829dd17be809ff
ElenaBakova <lenabakova72@gmail.com>
2021-10-03 00:04:34 +0300

    Init

---
+++ Semester3/Homework3/ThreadPoolTask/IMyTask.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ThreadPoolTask
+{
+    /// <summary>
+    /// Task interface
+    /// </summary>
+    /// <typeparam name="TResult">Result type</typeparam>
+    interface IMyTask<TResult>
+    {
+        /// <summary>
+        /// True if task is completed
+        /// </summary>
+        public bool IsCompleted { get; set; }
+
+        /// <summary>
+        /// Task result
+        /// </summary>
+        public TResult Result { get; set; }
+
+        /// <summary>
+        /// Creates a continuation
+        /// </summary>
+        /// <typeparam name="TNewResult">New result type</typeparam>
+        public void ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
---
+++ Semester3/Homework3/ThreadPoolTask/MyThreadPool.cs
@@ -0,0 +1,17 @@
+﻿namespace ThreadPoolTask
+{
+    /// <summary>
+    /// Thread pool class
+    /// </summary>
+    class MyThreadPool
+    {
+        /// <summary>
+        /// Thread pool constructor
+        /// </summary>
+        /// <param name="threadsCount">Number of threads</param>
+        public MyThreadPool(int threadsCount)
+        {
+
+        }
+    }
+}
