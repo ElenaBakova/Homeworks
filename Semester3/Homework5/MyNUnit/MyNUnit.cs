73e36f9bd31be1d8b677b99bb9ecd11218b7eb71
Elena Bakova <lenabakova72@gmail.com>
2022-03-05 21:47:14 +0300

    Changed

---
+++ Semester3/Homework5/MyNUnit/MyNUnit.cs
@@ -0,0 +1,76 @@
+﻿using Attributes;
+using System.Reflection;
+
+namespace MyNUnit
+{
+    /// <summary>
+    /// MyNUnit testing class
+    /// </summary>
+    class MyNUnit
+    {
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        /// <param name="path">Directory path</param>
+        public MyNUnit(string path)
+        {
+            var files = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
+            var classes = files.Distinct()
+                .Select(Assembly.Load)
+                .SelectMany(a => a.ExportedTypes)
+                .Where(t => t.IsClass);
+            var testClasses = classes.Where(c => c.GetMethods().Any(m => m.GetCustomAttributes().Any(a => a is TestAttribute)));
+            Parallel.ForEach(testClasses, t => StartTests(t));
+        }
+
+        private void StartTests(Type testClass)
+        {
+            var methods = SortByAttributes(testClass);
+        }
+
+        private MethodsList SortByAttributes(Type testClass)
+        {
+            var methodsList = new MethodsList();
+            foreach (var method in testClass.GetMethods())
+            {
+                foreach (var attribute in Attribute.GetCustomAttributes(method))
+                {
+                    switch (attribute)
+                    {
+                        case TestAttribute:
+                            methodsList.Test.Add(method);
+                            break;
+                        case AfterAttribute:
+                            methodsList.After.Add(method);
+                            break;
+                        case BeforeAttribute:
+                            methodsList.Before.Add(method);
+                            break;
+                        case AfterClassAttribute:
+                            methodsList.AfterClass.Add(method);
+                            break;
+                        case BeforeClassAttribute:
+                            methodsList.BeforeClass.Add(method);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return methodsList;
+        }
+
+        /// <summary>
+        /// Class for sorting methods by attributes
+        /// </summary>
+        private class MethodsList
+        {
+            public List<MethodInfo> After { get; } = new();
+            public List<MethodInfo> AfterClass { get; } = new();
+            public List<MethodInfo> Before { get; } = new();
+            public List<MethodInfo> BeforeClass { get; } = new();
+            public List<MethodInfo> Test { get; } = new();
+        }
+    }
+}
--- Semester3/Homework5/MyNUnit/Program.cs
+++ Semester3/Homework5/MyNUnit/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("Args should provide tests path");
+    return;
+}
+
+if (Directory.Exists(args[0]))
+{
+    Console.WriteLine("There's no such directory");
+    return;
+}
