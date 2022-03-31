693c89bd358d8ae3943ea8776a2891e4bf1be81a
Elena Bakova <lenabakova72@gmail.com>
2022-03-31 16:31:38 -0400

    Amended

--- Semester3/Homework5/MyNUnit/MyNUnit.cs
+++ Semester3/Homework5/MyNUnit/MyNUnit.cs
@@ -7,13 +7,9 @@ using System.Diagnostics;
 /// </summary>
 class MyNUnit
 {
-    List<TestResult> testsResult = new();
+    private static List<TestResult> testsResult = new();
 
-    /// <summary>
-    /// Class constructor
-    /// </summary>
-    /// <param name="path">Directory path</param>
-    public MyNUnit(string path)
+    public static void RunTesting(string path)
     {
         var files = Directory.GetFiles(path, "*.dll", SearchOption.AllDirectories);
         var classes = files.Distinct()
@@ -27,15 +23,42 @@ class MyNUnit
     /// <summary>
     /// Starts testing
     /// </summary>
-    private void StartTests(Type testClass)
+    private static void StartTests(Type testClass)
     {
         var methods = SortByAttributes(testClass);
         if (!RunMethods(methods.BeforeClass, methods.Test))
         {
             return;
         }
-        Parallel.ForEach(methods.Test, test => RunTest(test, testClass));
-        RunMethods(methods.BeforeClass, methods.Test);
+        foreach (var test in methods.Test)
+        {
+            if (!RunBeforeAfterTestMethod(methods.Before, testClass))
+            {
+                testsResult.Add(new TestResult(test.Name, ResultState.Ignored, TimeSpan.Zero, "Before test method failed"));
+                continue;
+            }
+
+            var result = RunTest(test, testClass);
+            if (result.Result == ResultState.Ignored)
+            {
+                testsResult.Add(result);
+                continue;
+            }
+            else if (result.Result == ResultState.Failed)
+            {
+                testsResult.Add(result);
+            }
+
+            if (!RunBeforeAfterTestMethod(methods.After, testClass))
+            {
+                testsResult.Add(new TestResult(test.Name, ResultState.Failed, TimeSpan.Zero, null));
+            }
+            else
+            {
+                testsResult.Add(result);
+            }
+        }
+        RunMethods(methods.AfterClass, methods.Test);
     }
 
     /// <summary>
@@ -44,7 +67,7 @@ class MyNUnit
     /// <param name="methods">Before or After class methods</param>
     /// <param name="tests">Test methods</param>
     /// <returns>Whether it's failed</returns>
-    private bool RunMethods(List<MethodInfo> methods, List<MethodInfo> tests)
+    private static bool RunMethods(List<MethodInfo> methods, List<MethodInfo> tests)
     {
         try
         {
@@ -54,7 +77,7 @@ class MyNUnit
         {
             foreach (var test in tests)
             {
-                testsResult.RemoveAll(t => t.Name == test.Name);
+                testsResult.RemoveAll(item => item.Name == test.Name);
                 testsResult.Add(new TestResult(test.Name, ResultState.Ignored, TimeSpan.Zero, "Before or After class method failed"));
             }
             return false;
@@ -67,47 +90,59 @@ class MyNUnit
     /// </summary>
     /// <param name="method">Test method</param>
     /// <param name="testClass">Test method class</param>
-    private void RunTest(MethodInfo method, Type testClass)
+    private static TestResult RunTest(MethodInfo method, Type testClass)
     {
         var properties = (TestAttribute?)method.GetCustomAttribute(typeof(TestAttribute));
         if (properties?.Ignore != null)
         {
-            testsResult.Add(new TestResult(method.Name, ResultState.Ignored, TimeSpan.Zero, properties.Ignore));
-            return;
+            return new TestResult(method.Name, ResultState.Ignored, TimeSpan.Zero, properties.Ignore);
         }
 
         var testClassInstance = Activator.CreateInstance(testClass);
         var stopwatch = new Stopwatch();
-        //var result = ResultState.Passed;
         try
         {
             stopwatch.Start();
             method.Invoke(testClassInstance, null);
             stopwatch.Stop();
+            if (properties?.Expected != null)
+            {
+                return new TestResult(method.Name, ResultState.Failed, stopwatch.Elapsed, null);
+            }
         }
         catch (Exception e)
         {
             stopwatch.Stop();
-            var result = ResultState.Passed;
             if (properties?.Expected != null && e.InnerException != properties.Expected)
             {
-                result = ResultState.Failed;
+                return new TestResult(method.Name, ResultState.Failed, stopwatch.Elapsed, null);
             }
-            testsResult.Add(new TestResult(method.Name, result, stopwatch.Elapsed, null));
-            return;
         }
-        if (properties?.Expected != null)
+        return new TestResult(method.Name, ResultState.Passed, stopwatch.Elapsed, null);
+    }
+
+    private static bool RunBeforeAfterTestMethod(List<MethodInfo> methods, Type testClass)
+    {
+        var testClassInstance = Activator.CreateInstance(testClass);
+        foreach (var method in methods)
         {
-            //result = ResultState.Failed;
-            testsResult.Add(new TestResult(method.Name, ResultState.Failed, stopwatch.Elapsed, null));
+            try
+            {
+                method.Invoke(testClassInstance, null);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
+        return true;
     }
 
     /// <summary>
     /// Sorts methods by attributes
     /// </summary>
     /// <returns>Class containing lists</returns>
-    private MethodsList SortByAttributes(Type testClass)
+    private static MethodsList SortByAttributes(Type testClass)
     {
         var methodsList = new MethodsList();
         foreach (var method in testClass.GetMethods())
@@ -126,9 +161,17 @@ class MyNUnit
                         methodsList.Before.Add(method);
                         break;
                     case AfterClassAttribute:
+                        if (!method.IsStatic)
+                        {
+                            throw new InvalidOperationException("After class methods should be static");
+                        }
                         methodsList.AfterClass.Add(method);
                         break;
                     case BeforeClassAttribute:
+                        if (!method.IsStatic)
+                        {
+                            throw new InvalidOperationException("Before class methods should be static");
+                        }
                         methodsList.BeforeClass.Add(method);
                         break;
                     default:
--- Semester3/Homework5/MyNUnit/Program.cs
+++ Semester3/Homework5/MyNUnit/Program.cs
@@ -7,3 +7,6 @@ if (Directory.Exists(args[0]))
 {
     throw new ArgumentException("There's no such directory");
 }
+
+var myUnit = new MyNUnit();
+MyNUnit.RunTesting(args[0]);
